a24386071dee57516a1350a7ff0f4f1e65f0e28c
Szymon Łukawski <szymon@lukaw.com>
2024-12-03 19:04:28 +0100

    test: spreadsheet instance creation

---
+++ test_spreadsheet.py
@@ -0,0 +1,8 @@
+from spreadsheet import Spreadsheet
+
+
+def test_spreadsheet_creation():
+    sheet = Spreadsheet((1,2))
+    assert sheet.n_columns == 1
+    assert sheet.n_rows == 2
+    assert sheet.grid == [[None],[None]]
