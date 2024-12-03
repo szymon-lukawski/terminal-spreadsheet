57a51dceed523cf4134b43e9eee401bf4aaeeeca
Szymon Łukawski <szymon@lukaw.com>
2024-12-03 19:00:48 +0100

    feat: Basic spreadsheet class created

---
+++ spreadsheet.py
@@ -0,0 +1,15 @@
+
+class Cell:
+    pass
+
+class Spreadsheet:
+    def __init__(self, dims: tuple[int, int]):
+        self.n_columns, self.n_rows = dims
+        self.grid : list[list[Cell]] = None
+        self._create_empty_grid(*dims)
+
+    def _create_empty_grid(self, n_columns, n_rows):
+        self.grid = [[None for col_i in range(n_columns)] for row_i in range(n_rows)]
+
+    
+
